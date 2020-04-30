7428d63de664fbbb4e61880b21ad789bf366ff34
DM Liao <dl.amorphous@gmail.com>
2020-04-30 02:38:47 -0400

    Go back to being buildless

--- tools/build.js
+++
@@ -1,5 +0,0 @@
-const buildDist = require('./dist')
-
-buildDist();
-
-console.log('Files built successfully. They can be found in the `./dist` folder')
--- tools/zip.js
+++ tools/zip.js
@@ -1,26 +1,21 @@
 const path = require('path')
 const fs = require('fs')
 const { spawnSync } = require('child_process')
-const buildDist = require('./dist')
 
 const buildDir = path.resolve(__dirname, '..', 'build');
 if (!fs.existsSync(buildDir)) {
 	fs.mkdirSync(buildDir, { recursive: true })
 }
 
-const buildPath = path.resolve(buildDir, 'build.zip')
+const buildPath = path.resolve(buildDir, 'build-raw.zip')
 if (fs.existsSync(buildPath)) {
 	fs.unlinkSync(buildPath)
 }
 
-buildDist();
-
 const zip = spawnSync('7z', ['a', '-tzip', path.resolve(buildPath), 
-	path.resolve(__dirname, '..', 'dist', 'index.html'),
-	path.resolve(__dirname, '..', 'dist', 'index.js'),
-	path.resolve(__dirname, '..', 'dist', 'dither-palette.png'),
-	path.resolve(__dirname, '..', 'dist', 'shader.frag'),
-	path.resolve(__dirname, '..', 'src', 'vendor'),
+	path.resolve(__dirname, '..', 'index.html'),
+	path.resolve(__dirname, '..', 'src'),
+	path.resolve(__dirname, '..', 'pwa'),
 	path.resolve(__dirname, '..', 'styles')
 ]);
 
